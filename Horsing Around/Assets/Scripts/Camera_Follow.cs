8649b2b19262530abdff00f66d3e466d89ebe047
Mitchell Dixon <mitchelldixon35@gmail.com>
2020-02-04 17:40:09 +0000

    Player controls + Camera follow
    Made player controls for horse
    Set up idle, walk and run anims/transitions
    Camera follows player

---
+++ Horsing Around/Assets/Scripts/Camera_Follow.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Camera_Follow : MonoBehaviour
+{
+    // Target to follow
+    public Transform Target;
+    public Vector3 PositionOffset;
+    public float RotationSpeed = 1f;
+
+
+    public float OffsetMagnitude = 1f; // How far behind the player should it be
+
+    float RotationX = 20f;
+
+    Vector3 NewPos;
+
+    void Start()
+    {
+        NewPos = new Vector3(Target.position.x + PositionOffset.x, Target.position.y + PositionOffset.y, Target.position.z + PositionOffset.z);
+        transform.position = NewPos;
+    }
+
+    void Update()
+    {
+        // Set position = target position + offset
+        NewPos = new Vector3(Target.position.x + PositionOffset.x, Target.position.y + PositionOffset.y, Target.position.z + PositionOffset.z);
+        transform.position = NewPos;
+
+        //// Get behind player
+        //Vector3 behindVec = RailsToFollow.position - CenterRef.position;
+
+        //// Multiply behind vec by magnitude
+        //behindVec = new Vector3(behindVec.x * OffsetMagnitude, behindVec.y * OffsetMagnitude, behindVec.z * OffsetMagnitude);
+
+        //// Set position = rails position + offset
+        //transform.position = new Vector3(RailsToFollow.position.x + behindVec.x, RailsToFollow.position.y + behindVec.y, RailsToFollow.position.z + behindVec.z);
+
+        //// Camera shouldnt be below rails
+        //if (RailsToFollow.position.y > transform.position.y)
+        //{
+        //    // Calc distance on y axis from rails
+        //    float distY = RailsToFollow.position.y - transform.position.y;
+
+        //    // Set new position
+        //    transform.position = new Vector3(transform.position.x + OffsetMagnitude, transform.position.y + distY, transform.position.z + OffsetMagnitude);
+        //}
+
+        //// Face the center
+        //transform.LookAt(CenterRef);
+    }
+}
---
+++ Horsing Around/Assets/Scripts/Player_Move.cs
@@ -0,0 +1,90 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+[RequireComponent(typeof(Rigidbody))]
+[RequireComponent(typeof(Animator))]
+public class Player_Move : MonoBehaviour
+{
+    public float RotationSpeed = 1f;
+    public float MoveSpeed = 1f;
+    public float SprintMultiplier = 2f; // Multiplier for sprint speed
+    float SprintMultiplierDefault = 1f; // Default value for multiplier
+    float CurrentSprintMultiplier = 1f;
+
+    //public float MinRotationY = 0f;
+    //public float MaxRotationY = 45f;
+
+    GameObject CameraRef;
+    Rigidbody Rigid;
+    Animator Anim;
+    
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        // Init camera
+        if (Camera.main != null)
+        {
+            CameraRef = Camera.main.gameObject;
+        }
+        else
+        {
+            Debug.Log("No main camera found! Player move needs it to work");            
+        }
+
+        // Init rigid
+        Rigid = GetComponent<Rigidbody>();
+        // Init animator
+        Anim = GetComponent<Animator>();
+    }
+    
+    void FixedUpdate()
+    {
+        // Get direction input
+        float x = Input.GetAxis("Horizontal") * -1f;
+        float z = Input.GetAxis("Vertical") * -1f;
+
+        // Calc direction
+        Vector3 dir = x * Vector3.right + z * Vector3.forward;
+        dir = dir.normalized; // Normalize direction (so diagonal isnt faster)
+
+        // Set sprint multiplier
+        CurrentSprintMultiplier = Input.GetKey(KeyCode.LeftShift) ? SprintMultiplier : SprintMultiplierDefault;
+
+        // Set speed
+        dir = dir * MoveSpeed * CurrentSprintMultiplier; // Apply sprint speed
+        dir.y = Rigid.velocity.y; // Dont want the player to change velocity on the y;
+
+        // Set velocity = direction
+        Rigid.velocity = dir;
+
+        // Rotate towards velocity
+        Vector3 target = transform.position + Rigid.velocity;
+
+        // Set target rotation
+        Vector3 targetRot = target - transform.position;
+        targetRot.y = 0f;
+        //targetRot.y = Mathf.Clamp(targetRot.y, MinRotationY, MaxRotationY);
+
+        // Only rotate if target rotation is not zero
+        if(targetRot != Vector3.zero)
+        {
+            transform.rotation = Quaternion.RotateTowards(transform.rotation, Quaternion.LookRotation(targetRot), RotationSpeed);
+        }       
+
+        // Set current max speed to the velocity magnitude
+        float curMaxSpeed = Rigid.velocity.magnitude;
+
+        // Clamp max speed between 0 and 1
+        curMaxSpeed = Mathf.Clamp(curMaxSpeed, 0f, 1f);
+
+        // Tell the animator the speed for transitions
+        Anim.SetFloat("Speed", curMaxSpeed);
+
+        // Set sprint bool in animator
+        bool isSprinting = CurrentSprintMultiplier > SprintMultiplierDefault ? true : false;
+        Anim.SetBool("IsSprinting", isSprinting);
+    }
+}
