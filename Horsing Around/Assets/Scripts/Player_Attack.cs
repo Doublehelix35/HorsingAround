3c7d0bde053ba3cf849a0beed534b2c2f1838d1b
Mitchell Dixon <mitchelldixon35@gmail.com>
2020-02-18 12:09:01 +0000

    Player attack set up
    Player attack Input
    Attack anim + transitions
    Lower/upper body avatar masks
    Enemy can be destroyed

---
+++ Horsing Around/Assets/Scripts/Player_Attack.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player_Attack : MonoBehaviour
+{
+    public float AttackLength = 0.1f; // How long an attack lasts for
+
+    float LastAttackTime; // Stores the time of the last attack
+    BoxCollider AttackCol; // Attack collider on this object
+    Animator PlayerAnimator; // Animator on parent
+
+    void Start()
+    {
+        // Init attack collider and disable it
+        AttackCol = GetComponent<BoxCollider>();
+        AttackCol.enabled = false;
+
+        // Init Player animator
+        PlayerAnimator = transform.parent.GetComponent<Animator>();
+
+        // Init last attack time
+        LastAttackTime = Time.time;
+    }
+    
+    void Update()
+    {
+        if(LastAttackTime + AttackLength < Time.time)
+        {
+            // Reset attack
+            AttackCol.enabled = false;
+
+            // Set is attacking to false
+            PlayerAnimator.SetBool("IsAttacking", false);
+        }
+
+        // Attack (only if attack col is disabled)
+        if (Input.GetKeyDown(KeyCode.X) && AttackCol.enabled == false) 
+        {
+            // Turn on attack collider
+            AttackCol.enabled = true;
+
+            // Set is attacking to true
+            PlayerAnimator.SetBool("IsAttacking", true);
+
+            // Set last attack time
+            LastAttackTime = Time.time;
+        }        
+    }
+
+    void OnTriggerEnter(Collider col)
+    {
+        if(col.gameObject.tag == "Enemy")
+        {
+            // Destroy enemy
+            Destroy(col.gameObject);
+        }
+    }
+}
