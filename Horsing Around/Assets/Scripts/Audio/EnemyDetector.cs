3358c9f0e1c1e0e9c9ff62f8d95cbdc8e6f97d5a
Mitchell Dixon <mitchelldixon35@gmail.com>
2020-05-08 11:52:31 +0100

    Enemy detector audio

---
+++ Horsing Around/Assets/Scripts/Audio/EnemyDetector.cs
@@ -0,0 +1,79 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyDetector : MonoBehaviour
+{
+    List<GameObject> EnemiesDetected;
+    SoundManager SoundManagerRef;
+    float EnemyDetectedVolume = 1f;
+    float NormalVolume = 0.5f;
+
+    float NullCheckDelay = 0.2f;
+
+    IEnumerator coroutine;
+
+    void Start()
+    {
+        // Init
+        SoundManagerRef = GameObject.FindGameObjectWithTag("GameController").GetComponent<SoundManager>();
+        EnemiesDetected = new List<GameObject>();
+
+        // Start null check
+        coroutine = NullCheck();
+        StartCoroutine(coroutine);
+
+        // Set volume
+        SoundManagerRef.SetMusicVolumeModifier(NormalVolume);
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if(other.tag == "Enemy")
+        {
+            EnemiesDetected.Add(other.gameObject);
+
+            // Only do once
+            if(EnemiesDetected.Count == 1)
+            {
+                SoundManagerRef.SetMusicVolumeModifier(EnemyDetectedVolume);
+            }
+        }
+    }
+
+    void OnTriggerExit(Collider other)
+    {
+        if (other.tag == "Enemy")
+        {
+            EnemiesDetected.Remove(other.gameObject);
+
+            // Only do once
+            if (EnemiesDetected.Count == 0)
+            {
+                SoundManagerRef.SetMusicVolumeModifier(NormalVolume);
+            }
+        }
+    }
+
+    IEnumerator NullCheck()
+    {
+        while (true)
+        {
+            yield return new WaitForSeconds(NullCheckDelay);
+
+            for(int i = 0; i < EnemiesDetected.Count; i++)
+            {
+                if(EnemiesDetected[i] == null)
+                {
+                    EnemiesDetected.Remove(EnemiesDetected[i]);
+
+                    if (EnemiesDetected.Count == 0)
+                    {
+                        SoundManagerRef.SetMusicVolumeModifier(NormalVolume);
+                    }
+                }
+            }
+
+        }
+    }
+}
---
+++ Horsing Around/Assets/Scripts/Audio/SoundManager.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SoundManager : MonoBehaviour
+{
+    public AudioSource MusicSource;
+
+    float VolumeCurrent = 1f;
+    float VolumeMax = 1f;
+    float VolumeModifier = 1f;
+
+    
+    internal void SetMusicVolumeModifier(float volumeModifier)
+    {
+        // Clamp between 0 and 1
+        VolumeModifier = Mathf.Clamp01(volumeModifier);
+
+        UpdateMusicVolume();
+    }
+
+    void UpdateMusicVolume()
+    {
+        VolumeCurrent = VolumeMax * VolumeModifier;
+        MusicSource.volume = VolumeCurrent;
+    }
+}
